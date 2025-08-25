d31f8cf69b231dc2fef5f7bdd13e520d14f6846b
RAHBAR SAMIR <rahbarsamir@gmail.com>
2025-08-25 13:27:54 +0530

    add-- AuthHeader added

---
+++ frontend/src/components/SignupComp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import AuthHeader from './AuthHeader'
+const SignupComp = () => {
+  return (
+    <div className=' flex-1 h-full '>
+        <AuthHeader firstText="biz" secondText="scan" />
+    </div>
+  )
+}
+
+export default SignupComp
--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Banner from '../components/Banner'
 import LoginComp from '../components/LoginComp'
+import SignupComp from '../components/SignupComp'
 const Auth = () => {
   return (
     <>
       <div className='flex w-full h-screen'>
-        <LoginComp />
+        {/* <LoginComp /> */}
+        <SignupComp/>
         <Banner />
       </div>
     </>
