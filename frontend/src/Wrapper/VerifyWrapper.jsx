873155fa912c102d97d97fca6c0655532958a373
RAHBAR SAMIR <rahbarsamir@gmail.com>
2025-08-27 18:29:19 +0530

    fix-- error while deploy

--- frontend/src/Wrapper/VerifyWrapper.jsx
+++ frontend/src/Wrapper/VerifyWrapper.jsx
@@ -1,12 +1,12 @@
 import { Navigate } from "react-router-dom";
 // import { useVerify } from "../hooks/useVerify";
 import React from "react";
-import { useVerify } from "../hooks/useVerify";
+
 // import jwtDecode from "jwt-decode";
 export default function ProtectedRoute({ children }) {
   const token = localStorage.getItem("authToken");
  
-  const { userData, setUserData } = useVerify();
+
 
 
 
